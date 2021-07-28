3e599366b1ab9b407bfdefd891c9247681fe3935
Mustafa Hassib <hassib@social-nuts.com>
2021-07-28 20:57:44 +0200

    Create Home and News Modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) }, { path: 'news', loadChildren: () => import('./news/news.module').then(m => m.NewsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
