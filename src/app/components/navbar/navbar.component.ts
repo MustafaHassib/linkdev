ef5cf4ba5999042bb82eb48ea896bb29e95a26df
Mustafa Hassib <hassib@social-nuts.com>
2021-07-30 22:55:09 +0200

    settings dropdown

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,23 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { Tooltip } from 'node_modules/bootstrap/dist/js/bootstrap.esm.min.js';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {
-  sidebarShow = true;
-  navHeight: any;
-  constructor(@Inject(DOCUMENT) private document: Document) {}
+export class NavbarComponent implements OnInit {
+  sidebarShow: boolean = false;
+  dropdownShow: boolean = false;
+
+  constructor() {}
+  ngOnInit() {
+    Array.from(
+      document.querySelectorAll('button[data-bs-toggle="tooltip"]')
+    ).forEach((tooltipNode) => new Tooltip(tooltipNode));
+  }
+  toggleMenu() {
+    console.log('sdfa');
+  }
 }
