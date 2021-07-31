68ac412c078274d3f90694638cb4d4a456b41feb
Mustafa Hassib <hassib@social-nuts.com>
2021-07-31 21:01:17 +0200

    Settings Meny

--- src/app/home/latest-news/latest-news.component.ts
+++ src/app/home/latest-news/latest-news.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-latest-news',
   templateUrl: './latest-news.component.html',
-  styleUrls: ['./latest-news.component.scss']
+  styleUrls: ['./latest-news.component.scss'],
 })
 export class LatestNewsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
